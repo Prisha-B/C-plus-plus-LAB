d6ac21d846a36d386e94beec60d3546f5fd6211f
Prisha-B <prishaburagohain@gmail.com>
2026-09-02 10:41:05 +0530

    Add files via upload

---
+++ call.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+//Call by reference
+
+void swap_ref(int *a, int *b)
+{
+    int temp= *a;
+    *a= *b;
+    *b= temp;
+}
+
+void swap_value(int p, int q)
+{
+    int temp= p;
+    p= q;
+    q= temp;
+    cout<< "After swap: a= " << p << ", b= " << q << endl;
+}
+
+int main()
+{
+    int x= 5;
+    int y= 10;
+    int a= 40;
+    int b= 50;
+    cout<< "Call by reference: "<< endl;
+    cout<< "Before swap: x= "<< x << ", y= " << y << endl;
+    swap_ref(&x, &y);
+    cout<< "After swap: x= " << x << ", y= " << y << endl;
+
+    cout<< "\n\nCall by value: "<< endl;
+    cout<< "Before swap: a= "<< a << ", b= " << b << endl;
+    swap_value(a, b);
+
+    return 0;
+}
